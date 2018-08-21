3409f45bd0eb832e23c60699267d5db0e798d10c
MarcelSchmidberger <marcel.schmidberger@student.hpi.de>
2018-08-21 17:35:34 +0200

    Initial working spring saml example

---
+++ src/main/java/com/marcelschmidberger/samlexample/SamlexampleApplication.java
@@ -0,0 +1,12 @@
+package com.marcelschmidberger.samlexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamlexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamlexampleApplication.class, args);
+	}
+}
